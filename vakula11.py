1550b5706728b57dfb8407b03df584c43274d79c
vakula1999 <ctcuser123@ip-172-31-46-179.us-east-2.compute.internal>
2023-11-18 01:02:49 +0000

    adding a python program

---
+++ vakula11.py
@@ -0,0 +1,19 @@
+# Lab 11
+# Vamsi Krishna Akula
+
+def main():
+    # Ask the user to input a number
+    number = float(input("Enter a number: "))
+
+    # Calculate the square of the number
+    squared_number = number ** 2
+
+    # Display the squared result
+    print(f"The square of {number} is: {squared_number}")
+
+    # Display a saying
+    print(" World is not gonna end")
+
+if __name__ == "__main__":
+    main()
+
